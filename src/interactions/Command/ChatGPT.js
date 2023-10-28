f0c4cacafc79862132dadb64659070bb703cf993
Yoav Levy <yoavlevy1000@gmail.com>
2023-10-28 22:23:20 +0300

    added chatGPT command

---
+++ src/interactions/Command/ChatGPT.js
@@ -0,0 +1,24 @@
+const { CommandInteraction, Client } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
+const Discord = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('chatgpt')
+        .setDescription('play with chatgpt')
+        .addStringOption((option) => option.setName('text').setDescription('enter a text for chatgpt').setRequired(true))
+    ,
+
+    /** 
+     * @param {Client} client
+     * @param {CommandInteraction} interaction
+     * @param {String[]} args
+     */
+
+    run: async (client, interaction, args) => {
+        await interaction.deferReply({ fetchReply: true });
+        client.loadSubcommands(client, interaction, args);
+    },
+};
+
+ 
